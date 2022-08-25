7dbc9254f394dc824f5eb12c4eda13cfe1247dc3
Alex <89217796407@ya.ru>
2022-08-25 22:14:05 +0300

    Add DZ 3

--- Examp/Exam001/Program.cs
+++ Examp/Exam001/Program.cs
@@ -49,13 +49,9 @@ Console.Write("Введи цифру, обозначающую день неде
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
 if (dayNumber >= 1 && dayNumber <= 7)
-{
     Console.WriteLine(dayNumber == 6 || dayNumber == 7 ? "Да, это выходной" : "Нет, это не выходной");
-}
 else
-{
     Console.WriteLine("В недели 7 дней!");
-}
 */
 
 /*
@@ -77,3 +73,113 @@ else if (srAr % 10 == min /100){
 else{
     Console.WriteLine("Нет");}
 */
+
+
+/*
+Console.Clear();
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    Console.WriteLine("1");
+else if (x < 0 && y > 0)
+    Console.WriteLine("2");
+else if (x < 0 && y < 0)
+    Console.WriteLine("3");
+else if (x > 0 && y < 0)
+    Console.WriteLine("4");
+*/
+
+/*
+Console.Clear();
+Console.Write("Введите четверть: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x == 1)
+    Console.WriteLine($"Диапазон значений в {x} четверти: x > 0 и y > 0");
+else if (x == 2)
+    Console.WriteLine($"Диапазон значений в {x} четверти: x < 0 и y > 0");
+else if (x == 3)
+    Console.WriteLine($"Диапазон значений в {x} четверти: x < 0 и y < 0");
+else if (x == 4)
+    Console.WriteLine($"Диапазон значений в {x} четверти: x > 0 и y < 0");
+else
+    Console.WriteLine("Введите значение в диапазоне 1-4");
+*/
+
+/*
+//Задача 19.
+//Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3])
+    Console.WriteLine($"Число: {number} - палиндром.");
+  else Console.WriteLine($"Число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5)
+  CheckingNumber(number);
+else Console.WriteLine($"Введи правильное число");
+*/
+
+/*
+//Задача 21.
+//Напишите программу, которая принимает на вход координаты двух 
+//точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка:  {segmentLength}");
+*/
+
+/*
+//Задача 23. 
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (n > 0 && count <= n)
+{
+    Console.Write($"{Math.Pow(count, 3)}  ");
+    count ++;
+}
+Console.WriteLine();
+
+while (n < 0 && count >= n)
+{
+    Console.Write($"{Math.Pow(count, 3)}  ");
+    count --;
+}
+Console.WriteLine();
+*/
+
