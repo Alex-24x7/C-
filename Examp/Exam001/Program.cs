084f1ab413619ce0d5266c662b0616ed84e578dd
Alex <89217796407@ya.ru>
2022-09-08 21:35:54 +0300

    Seminar 7

--- Examp/Exam001/Program.cs
+++ Examp/Exam001/Program.cs
@@ -827,7 +827,7 @@ Console.WriteLine($"две прямые пересекутся в точке с 
 */
 
 
-
+/*
 //Дополнительная задача (задача со звёздочкой): Напишите программу, которая задаёт массив из n элементов, 
 //которые необходимо заполнить случайными значениями и сдвинуть элементы массива влево, или вправо на 1 позицию.
 //[8, 5, 1, 7, 0] - [5, 1, 7, 0, 8] - сдвиг влево
@@ -888,5 +888,183 @@ void PrintArray(int[] Numbers)
     Console.Write("]");
     Console.WriteLine();
 }
+*/
 
 
+/*
+//Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+
+Console.Clear();
+
+Console.Write("Количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+int [,] array = FillArray(rows, columns, 1 , 10);
+
+PrintArray(array);
+
+int [,] FillArray(int rows, int columns, int min, int max){
+    int[,] array = new int[rows,columns];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j< columns; j++){
+            array[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] filledArray){
+    for(int i = 0; i<filledArray.GetLength(0);i++){
+        for(int j = 0; j < filledArray.GetLength(1);j++){
+            Console.Write($"{filledArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+
+/*
+//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве 
+//находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+//m = 3, n = 4.
+Console.Clear();
+
+Console.Write("Количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+int [,] array = FillArray(rows, columns);
+PrintArray(array);
+
+
+int [,] FillArray(int rows, int columns){
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j< columns; j++){
+            array[i,j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] filledArray){
+    for(int i = 0; i<filledArray.GetLength(0);i++){
+        for(int j = 0; j < filledArray.GetLength(1);j++){
+            if (filledArray[i,j] / 10 == 0)
+            Console.Write($"{filledArray[i,j]}  ");
+            else Console.Write($"{filledArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+
+/*
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+Console.Clear();
+
+Console.Write("Количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+int [,] array = FillArray(rows, columns, 1 , 10);
+
+PrintArray(array);
+Console.WriteLine();
+PrintArray(FillArrayCheck(array));
+
+int [,] FillArrayCheck(int [,] array){
+    for(int i = 1; i < rows; i++){
+        for(int j = 1; j< columns; j++){
+            if (i % 2 == 1 && j % 2 == 1){
+                array[i,j] *= array[i,j];
+            }
+        }
+    }
+    return array;
+}
+
+int [,] FillArray(int rows, int columns, int min, int max){
+    int[,] array = new int[rows,columns];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j< columns; j++){
+            array[i,j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] filledArray){
+    for(int i = 0; i<filledArray.GetLength(0);i++){
+        for(int j = 0; j < filledArray.GetLength(1);j++){
+            if (filledArray[i,j] / 10 == 0)
+            Console.Write($"{filledArray[i,j]}  ");
+            else Console.Write($"{filledArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+
+
+// Задача 51: Найти сумму по главной диагонали.
+Console.Clear();
+
+Console.Write("Количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+int [,] array = FillArray(rows, columns, -9 , 10);
+
+PrintArray(array);
+Console.WriteLine();
+DiagonalArray(array);
+
+void DiagonalArray(int [,] array){
+    int[] Sum = new int[Math.Min(rows,columns)];
+    int sum = 0;
+    for(int i = 0; i < Math.Min(rows,columns); i++){
+        sum += array[i, i];
+        Sum[i] = array[i, i];
+                
+        }
+Console.WriteLine($"Сумма элементов диагонали:");
+Console.Write($"({String.Join(") + (", Sum)})");
+Console.WriteLine($" = {sum}");
+}
+
+int [,] FillArray(int rows, int columns, int min, int max){
+    int[,] array = new int[rows,columns];
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j< columns; j++){
+            array[i,j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] filledArray){
+    for(int i = 0; i<filledArray.GetLength(0);i++){
+        for(int j = 0; j < filledArray.GetLength(1);j++){
+            if (filledArray[i,j] >= 0)
+            Console.Write($"{filledArray[i,j]}   ");
+            else
+            Console.Write($"{filledArray[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
