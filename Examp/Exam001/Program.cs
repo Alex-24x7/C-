622d14dd2c519c81aa54320aafb4059f87323f82
AlexWork <>
2022-09-09 11:39:57 +0300

    Add DZ_7

--- Examp/Exam001/Program.cs
+++ Examp/Exam001/Program.cs
@@ -1018,7 +1018,7 @@ void PrintArray(int [,] filledArray){
 */
 
 
-
+/*
 // Задача 51: Найти сумму по главной диагонали.
 Console.Clear();
 
@@ -1068,3 +1068,144 @@ void PrintArray(int [,] filledArray){
         Console.WriteLine();
     }
 }
+*/
+
+
+
+// //Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// //m = 3, n = 4.
+// //0,5 7 -2 -0,2
+// //1 -3,3 8 -9,9
+// //8 7,8 -7,1 9
+
+// Console.Clear();
+// Console.WriteLine("Введите количество строк:");
+// int linesVol = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int columnsVol = Convert.ToInt32(Console.ReadLine());
+// double[,] numbers = new double[linesVol, columnsVol];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i,j] >= 0)
+//             Console.Write($"{array[i,j]}   ");
+//             else
+//             Console.Write($"{array[i,j]}  ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+
+
+
+
+// //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// //и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Console.Clear();
+// Console.WriteLine("Введите номер строки:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите номер столбца:");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [,] numbers = new int [10,10];
+// FillArrayRandomNumbers(numbers);
+
+// if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+//     Console.WriteLine("такого элемента нет");
+// else
+//     Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         {        
+//             for (int j = 0; j < array.GetLength(1); j++)
+//                 array [i,j] = new Random().Next(-100, 100)/10;
+//         }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i,j] >= 0)
+//             Console.Write($"{array[i,j]}   ");
+//             else
+//             Console.Write($"{array[i,j]}  ");
+//         }   
+//         Console.Write("]");
+//         Console.WriteLine(""); 
+//     }
+// }
+
+
+
+// //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Console.Clear();
+// Console.WriteLine("Введите количество строк:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// int[,] numbers = new int[n, m];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine();
+// PrintArray(numbers);
+
+// for (int j = 0; j < numbers.GetLength(1); j++)
+// {
+//     double avarage = 0;
+//     for (int i = 0; i < numbers.GetLength(0); i++)
+//         avarage = (avarage + numbers[i, j]);
+//     avarage = avarage / n;
+//     Console.Write(avarage + "; ");
+// }
+
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(0, 10);
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i,j] >= 0)
+//             Console.Write($"{array[i,j]}   ");
+//             else
+//             Console.Write($"{array[i,j]}  ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
